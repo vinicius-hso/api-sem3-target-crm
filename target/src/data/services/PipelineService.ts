6d729f83b94a9617c6de921b54419e42edade782
Matheus Costa <matheus271200.ms@gmail.com>
2021-09-11 21:10:43 -0300

    feat: creating service to execute api methods

---
+++ target/src/data/services/PipelineService.ts
@@ -0,0 +1,40 @@
+import { serviceApi as api } from "./serviceApi";
+
+class PipelineService {
+  private headers: object;
+
+  async deletePipeline(id: string): Promise<object> {
+    try {
+      const response = await api.delete(`/pipeline/${id}`, {
+        headers: this.headers,
+      });
+
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updatePipeline(id: string, name: string): Promise<object> {
+    const body: string = name;
+
+    try {
+      const response = await api.put(`/pipeline/${id}`, body, {
+        headers: this.headers,
+      });
+
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  constructor() {
+    const token = localStorage.getItem("@target:token");
+    this.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+}
+
+export default new PipelineService();
