1d05d0cd0555551f00dd3a2ce36e0942796b176e
Sarah Fernandes <fernandessarah.06@gmail.com>
2021-09-12 11:43:56 -0300

    perf: switch order

--- target/src/types/Modal.ts
+++ target/src/types/Modal.ts
@@ -1,8 +1,8 @@
 export default interface ModalTypes {
+  createModalState: boolean;
+  useCreateModal: () => void;
   updateModalState: boolean;
   useUpdateModal: () => void;
   deleteModalState: boolean;
   useDeleteModal: () => void;
-  createModalState: boolean;
-  useCreateModal: () => void;
 }
