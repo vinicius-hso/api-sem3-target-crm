44ac696393af3a3030caa44ff80892676a0153b6
Matheus Costa <matheus271200.ms@gmail.com>
2021-09-16 23:04:49 -0300

    feat: creating deal types

---
+++ target/src/types/Deal.ts
@@ -0,0 +1,12 @@
+export interface DealTypes {
+  dealName: string;
+  company: string;
+  contact: string;
+  email: string;
+  phone: string;
+  porpose: string;
+  startDate: string;
+  price: number;
+  tag: string;
+  endDate: string;
+}
