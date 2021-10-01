02251eabac9df98f80ae32703e7d089520fce713
Matheus Costa <matheus271200.ms@gmail.com>
2021-09-30 21:15:59 -0300

    feat: creating types for contact

---
+++ target/src/types/Contact.ts
@@ -0,0 +1,13 @@
+export interface IContact {
+  id?: string;
+  name: string;
+  email: string;
+  phone: string;
+  city: string;
+  state: string;
+  company_id?: string;
+  tag?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}
