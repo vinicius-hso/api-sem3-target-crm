8ab836779666ccfe6efb6479148ab539000167e5
Matheus Costa <matheus271200.ms@gmail.com>
2021-10-05 11:50:55 -0300

    feat: adding context to handle update modal

--- target/src/contexts/ContactContext.tsx
+++ target/src/contexts/ContactContext.tsx
@@ -5,16 +5,24 @@ const ContactContext = createContext<IContactContext>({} as IContactContext);
 
 export const ContactProvider: React.FC = ({ children }) => {
   const [createContactModal, setCreateContactModal] = useState<boolean>(false);
+  const [updateContactModal, setUpdateContactModal] = useState<boolean>(false);
 
   const useCreateContactModal = () => {
     setCreateContactModal(!createContactModal);
   };
+
+  const useUpdateContactModal = () => {
+    setUpdateContactModal(!updateContactModal);
+  };
+
 
   return (
     <ContactContext.Provider
       value={{
         useCreateContactModal,
         createContactModal,
+        useUpdateContactModal,
+        updateContactModal
       }}
     >
       {children}
