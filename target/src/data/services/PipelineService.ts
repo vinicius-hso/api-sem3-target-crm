deb80c61a026ab7da2aeb2d41b383dac16f004e5
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2021-11-03 23:06:35 -0300

    add a toast for pipeline

--- target/src/data/services/PipelineService.ts
+++ target/src/data/services/PipelineService.ts
@@ -2,6 +2,8 @@ import React from "react";
 import { DealTypes } from "types/Deal";
 import { pipeline } from "types/Modal";
 import { serviceApi as api } from "./ServiceApi";
+import { toast } from "react-toastify";
+
 class PipelineService {
   async getPiplines(): Promise<pipeline[]> {
     try {
@@ -9,6 +11,9 @@ class PipelineService {
 
       return data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
@@ -19,6 +24,9 @@ class PipelineService {
 
       return data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
@@ -28,9 +36,13 @@ class PipelineService {
 
     try {
       const response = await api.post("/pipeline/", body);
-
+      
+      toast.success("Pipeline criado com sucesso!");
       return response.data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
@@ -41,8 +53,12 @@ class PipelineService {
     try {
       const response = await api.put(`/pipeline/${id}`, body);
 
+      toast.success("Pipeline alterado com sucesso!");
       return response.data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
@@ -51,8 +67,12 @@ class PipelineService {
     try {
       const response = await api.delete(`/pipeline/${id}`);
 
+      toast.success("Pipeline deletado com sucesso!");
       return response.data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
@@ -61,8 +81,12 @@ class PipelineService {
     try {
       const response = await api.post("/deal/", data);
 
+      toast.success("Deal criado com sucesso!");
       return response.data;
     } catch (error) {
+      toast.error(
+        "Ops! algo deu errado, verifique sua conexão e tente novamente."
+      );
       return error;
     }
   }
